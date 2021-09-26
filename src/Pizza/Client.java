d435d102f2f5ec48eefe7710b3b9fcd0e3560969
AlexNik13 <4pnav21@gmail.com>
2021-09-26 21:56:58 +0300

    Implemented Addition of ingredients

--- src/Pizza/Client.java
+++ src/Pizza/Client.java
@@ -1,5 +1,6 @@
 package Pizza;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Client {
@@ -64,6 +65,64 @@ public class Client {
 
         newZakaz[pizza.length] = new Pizza();
         newZakaz[pizza.length].createPizza(addPizza);
+
+
+        System.out.println("\n Добавить ингредиенты yes. Enter to continue.");
+        String str = new String(in.next());
+        str = str.toLowerCase();
+
+        if (str.equals("yes")){
+            newZakaz[pizza.length].addName("*");
+        }
+
+        while (str.equals("yes")) {
+
+            System.out.println("Добавлякм");
+            System.out.println("1. Сыр");
+            System.out.println("2. Мяска");
+            System.out.println("3. Бекон");
+            System.out.println("4. Грибы");
+            System.out.println("5. Ветчина");
+            System.out.println("6. Болгарского перец");
+            System.out.println("7. Брокколи");
+            System.out.println("8. Оливки ");
+            System.out.println("9. Колбасы");
+            System.out.println("0. Хватит.");
+            int choice = in.nextInt();
+            switch (choice){
+                case 0:
+                    str = new String("no");
+                    break;
+                case 1:
+                    newZakaz[pizza.length].addCheeseGram(50);
+                    break;
+                case 2:
+                    newZakaz[pizza.length].addMeatGram(50);
+                    break;
+                case 3:
+                    newZakaz[pizza.length].addBaconGram(50);
+                    break;
+                case 4:
+                    newZakaz[pizza.length].addMushroomsGram(50);
+                    break;
+                case 5:
+                    newZakaz[pizza.length].addHamGram(50);
+                    break;
+                case 6:
+                    newZakaz[pizza.length].addBellPepperGram(50);
+                    break;
+                case 7:
+                    newZakaz[pizza.length].addBroccoliGram(50);
+                    break;
+                case 8:
+                    newZakaz[pizza.length].addOlivesGram(50);
+                    break;
+                case 9:
+                    newZakaz[pizza.length].addSausageGram(50);
+                    break;
+            }
+
+        }
 
 
         return newZakaz;
